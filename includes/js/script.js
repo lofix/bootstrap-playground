4a5a489da0c6afd4607468c5150c315fa3c3eb83
Maciek Milewski <maciekk.milewski@gmail.com>
2017-01-25 16:48:27 +0100

    added popover and tooltip

--- includes/js/script.js
+++ includes/js/script.js
@@ -4,6 +4,11 @@ $(function() {
         e.preventDefault();
         var successAlert = $("#success-alert");
         successAlert.slideToggle();
-    })
+    });
 
+    $("a.pop").click(function(e) {
+        e.preventDefault();
+    });
+    $("a.pop").popover();
+    $('[rel="tooltip"]').tooltip();
 });
